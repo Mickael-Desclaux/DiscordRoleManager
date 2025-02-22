20ffe5308d5ecbfbb76d848dfb301e0dea2a269b
Mickael <mickaeldesclaux@gmail.com>
2025-02-22 18:42:35 +0100

    project initialization

---
+++ src/main/java/com/discord/role-manager/App.java
@@ -0,0 +1,13 @@
+package com.discord.role-manager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
