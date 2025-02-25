e2f91f447f1e09129c9b3dee8223f1b3a33598a3
Mickael <mickaeldesclaux@gmail.com>
2025-02-25 17:28:50 +0100

    removed dotenv dependency

--- src/main/java/com/discord/role_manager/App.java
+++ src/main/java/com/discord/role_manager/App.java
@@ -1,18 +1,16 @@
 package com.discord.role_manager;
 
 import com.discord.role_manager.listeners.RoleManagementListener;
-import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 
 public class App
 {
-    Dotenv dotenv = Dotenv.load();
     private final JDA jda;
-    private String token = dotenv.get("BOT_TOKEN");
-    private String welcomeMessageId = dotenv.get("WELCOME_MESSAGE_ID");
-    private String presentationChannelId = dotenv.get("PRESENTATION_CHANNEL_ID");
+    private String token = System.getenv("BOT_TOKEN");
+    private String welcomeMessageId = System.getenv("WELCOME_MESSAGE_ID");
+    private String presentationChannelId = System.getenv("PRESENTATION_CHANNEL_ID");
 
     public App() {
         this.jda = JDABuilder.createDefault(token)
