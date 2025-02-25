157d5ca16a5d179306f89067fcb2d49744db50ee
Mickael <mickaeldesclaux@gmail.com>
2025-02-25 17:19:54 +0100

    Update gitignore and pom.xml

--- src/main/java/com/discord/role_manager/App.java
+++ src/main/java/com/discord/role_manager/App.java
@@ -1,18 +1,18 @@
 package com.discord.role_manager;
 
-import com.discord.role_manager.config.Config;
 import com.discord.role_manager.listeners.RoleManagementListener;
+import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 
 public class App
 {
+    Dotenv dotenv = Dotenv.load();
     private final JDA jda;
-    private String token = Config.getToken();
-    private String welcomeMessageId = Config.getWelcomeMessageId();
-    private String presentationChannelId = Config.getPresentationChannelId();
+    private String token = dotenv.get("BOT_TOKEN");
+    private String welcomeMessageId = dotenv.get("WELCOME_MESSAGE_ID");
+    private String presentationChannelId = dotenv.get("PRESENTATION_CHANNEL_ID");
 
     public App() {
         this.jda = JDABuilder.createDefault(token)
@@ -22,7 +22,6 @@ public class App
                         GatewayIntent.MESSAGE_CONTENT
                 )
                 .addEventListeners(new RoleManagementListener(token, welcomeMessageId, presentationChannelId))
-                .setActivity(Activity.playing("C'est un test"))
                 .build();
     }
 
